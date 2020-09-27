8299527435ef503cdf102c31a1efd037e5232cf6
AndrewZakharenka <zakharenka.andrew@gmail.com>
2020-09-27 18:19:21 +0300

    Last commit

--- src/HomeWork30092020/Cirlcles/Circle2.java
+++ src/HomeWork30092020/Cirlcles/Circle2.java
@@ -4,8 +4,8 @@ public class Circle2 {
     public static void main(String[] args) {
         int countAmebs = 1;
         for (int i = 1; i <= 24; i++) {
-            countAmebs *= 2;
             if (i % 3 == 0) {
+                countAmebs *= 2;
                 System.out.println("After " + i + " hours, the number of amoebas will be equal to: " + countAmebs);
             }
         }
